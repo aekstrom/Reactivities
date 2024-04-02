5bc8a585fd5c12be493301e8b5e006925583887e
Anders Ekström <anders.ekstrom@valtech.com>
2024-04-02 15:44:06 +0200

    End of section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class ActivitiesController(DataContext dbContext) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<IList<Activity>>> GetActivities()
+    {
+        return await dbContext.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+        return await dbContext.Activities.FindAsync(id);
+    }
+}
